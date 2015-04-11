66bd0020ff7496f89f4e4acd6c5f480dcc2dda62
bref <bref@brefdeMac-mini.local>
2015-04-11 13:56:10 +0800

    Initial Commit

---
+++ KeyBoardLinkMove/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KeyBoardLinkMove
+//
+//  Created by bref on 15/4/11.
+//  Copyright (c) 2015年 bref. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KeyBoardLinkMove/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KeyBoardLinkMove
+//
+//  Created by bref on 15/4/11.
+//  Copyright (c) 2015年 bref. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
